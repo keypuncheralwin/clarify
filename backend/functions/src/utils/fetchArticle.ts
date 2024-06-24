522abe66a809741354e2908a5d114e39a0733dbd
keypuncheralwin <alwingeorge11@gmail.com>
2024-06-24 22:37:49 +1000

    feat: Update the way the articles are fetched by using the extractus package

--- backend/functions/src/utils/fetchArticle.ts
+++ backend/functions/src/utils/fetchArticle.ts
@@ -1,51 +1,20 @@
-import axios from 'axios';
 import { Article } from '../types/article';
-import cheerio from 'cheerio';
 import logger from '../logger/logger';
+import { extract } from '@extractus/article-extractor';
 
-export default async function fetchArticle(
-  url: string
-): Promise<Article | null> {
-  try {
-    const { data: html } = await axios.get(url, {
-      headers: {
-        'User-Agent':
-          'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
-      },
-    });
-
-    const $ = cheerio.load(html);
-
-    const title =
-      $('meta[property="og:title"]').attr('content') ||
-      $('meta[name="twitter:title"]').attr('content') ||
-      $('head > title').text() ||
-      $('meta[name="title"]').attr('content');
-
-    const description =
-      $('meta[name="description"]').attr('content') ||
-      $('meta[property="og:description"]').attr('content') ||
-      $('meta[name="twitter:description"]').attr('content');
-
-    const content = $('article').text() || $('body').text();
-
-    if (!title && !content) {
-      logger.warn(`No article found at URL: ${url}`);
-      return null;
-    }
-
-    logger.info(`Article fetched successfully from URL: ${url}`, {
-      title,
-      description,
-    });
-
-    return {
-      title: (title as string).trim(),
-      subtitle: description?.trim() || '',
-      content: (content as string).trim(),
-    };
-  } catch (error) {
-    logger.error('Error fetching article:', error);
+export default async function fetchArticle(url: string): Promise<Article | null> {
+  const article = await extract(url);
+  if (!article?.title || !article?.content) {
+    logger.warn(`No article found at URL: ${url}`);
     return null;
   }
+  logger.info(`Article extracted successfully`);
+  logger.info(`Title: ${article.title}`);
+  const truncatedContent = article.content.length > 100 ? `${article.content.slice(0, 100)}...` : article.content;
+  logger.info(`Content: ${truncatedContent}`);
+  return {
+    title: article.title,
+    subtitle: article.description || '',
+    content: article.content,
+  };
 }
