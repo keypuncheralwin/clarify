b42b3277f2339a8090646ccec13cbd12ed642cac
keypuncheralwin <alwingeorge11@gmail.com>
2024-07-05 15:47:47 +1000

    chore: change logout button styling

--- app/lib/screens/account_screen.dart
+++ app/lib/screens/account_screen.dart
@@ -1,14 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:clarify/providers/theme_provider.dart';
 import 'package:clarify/widgets/sign_in_bottom_sheet.dart';
 
-class AccountScreen extends ConsumerWidget {
+class AccountScreen extends ConsumerStatefulWidget {
   const AccountScreen({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  _AccountScreenState createState() => _AccountScreenState();
+}
+
+class _AccountScreenState extends ConsumerState<AccountScreen> {
+  BuildContext? _bottomSheetContext;
+
+  @override
+  Widget build(BuildContext context) {
     final isDarkMode = ref.watch(themeProvider).brightness == Brightness.dark;
+    final user = FirebaseAuth.instance.currentUser;
 
     return Scaffold(
       body: SingleChildScrollView(
@@ -17,21 +26,22 @@ class AccountScreen extends ConsumerWidget {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             const SizedBox(height: 20),
-            Center(
-              child: ElevatedButton(
-                onPressed: () {
-                  _showEmailBottomSheet(context);
-                },
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.deepPurple,
-                  padding: const EdgeInsets.symmetric(horizontal: 32, vertical: 12),
-                ),
-                child: const Text(
-                  'Sign in or create an account',
-                  style: TextStyle(fontSize: 16, color: Colors.white),
+            if (user == null)
+              Center(
+                child: ElevatedButton(
+                  onPressed: () {
+                    _showEmailBottomSheet(context);
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.deepPurple,
+                    padding: const EdgeInsets.symmetric(horizontal: 32, vertical: 12),
+                  ),
+                  child: const Text(
+                    'Sign in or create an account',
+                    style: TextStyle(fontSize: 16, color: Colors.white),
+                  ),
                 ),
               ),
-            ),
             const SizedBox(height: 40),
             Text(
               'General Settings',
@@ -133,6 +143,26 @@ class AccountScreen extends ConsumerWidget {
               subtitle: '1.0.0', // Replace with actual app version
               isDarkMode: isDarkMode,
             ),
+            if (user != null)
+              _buildListTile(
+                context,
+                icon: Icons.logout,
+                title: 'Logout',
+                onTap: () async {
+                  await FirebaseAuth.instance.signOut();
+                  setState(() {
+                    _showBottomSheet('Logged out successfully');
+                  });
+                  Future.delayed(const Duration(seconds: 1), () {
+                    if (_bottomSheetContext != null) {
+                      Navigator.pop(_bottomSheetContext!);
+                      _bottomSheetContext = null;
+                    }
+                  });
+                },
+                isDarkMode: isDarkMode,
+                titleColor: const Color(0xFFfe2712), // Set the logout text color to red
+              ),
           ],
         ),
       ),
@@ -145,12 +175,13 @@ class AccountScreen extends ConsumerWidget {
       String? subtitle,
       Widget? trailing,
       void Function()? onTap,
-      required bool isDarkMode}) {
+      required bool isDarkMode,
+      Color titleColor = Colors.black}) {
     return ListTile(
       leading: Icon(icon, color: isDarkMode ? Colors.white : Colors.black),
       title: Text(
         title,
-        style: TextStyle(color: isDarkMode ? Colors.white : Colors.black),
+        style: TextStyle(color: title == 'Logout' ? Colors.red : (isDarkMode ? Colors.white : Colors.black)),
       ),
       subtitle: subtitle != null
           ? Text(
@@ -174,5 +205,31 @@ class AccountScreen extends ConsumerWidget {
         return const SignInBottomSheet();
       },
     );
+  }
+
+  void _showBottomSheet(String message) {
+    showModalBottomSheet(
+      context: context,
+      isDismissible: false,
+      builder: (BuildContext context) {
+        _bottomSheetContext = context;
+        return Container(
+          padding: const EdgeInsets.all(16.0),
+          height: 100,
+          child: Center(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Icon(Icons.check_circle, color: Colors.green, size: 24.0),
+                const SizedBox(width: 16),
+                Text(message),
+              ],
+            ),
+          ),
+        );
+      },
+    ).whenComplete(() {
+      _bottomSheetContext = null;
+    });
   }
 }
